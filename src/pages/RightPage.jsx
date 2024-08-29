298ad6e2de0a377a3b1c9bdea9c3e158dcba3331
Melto007 <meltosm8@gmail.com>
2024-08-29 11:43:49 +0530

    swiper for year and date is added

--- src/pages/RightPage.jsx
+++ src/pages/RightPage.jsx
@@ -3,13 +3,9 @@ import {
     Card,
     CardHeader,
     CardBody,
-    CardFooter,
-    Avatar,
-    AvatarGroup,
     Input,
     Button,
     Tabs,
-    Calendar,
     Tab
 } from '@nextui-org/react'
 import {
@@ -22,7 +18,71 @@ import { MdHolidayVillage, MdOutlineKeyboardArrowLeft, MdKeyboardArrowRight  } f
 import SpanComponents from '../components/SpanComponents';
 import Cards from '../components/Cards'
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination, Navigation } from 'swiper/modules';
+
+const getDatesForMonth = (year, month) => {
+    const dates = [];
+    const daysInMonth = new Date(year, month + 1, 0).getDate()
+
+    for (let day = 1; day <= daysInMonth; day++) {
+        const date = new Date(year, month, day);
+        dates.push({
+            dayOfWeek: date.toLocaleDateString('en-US', { weekday: 'short' }),
+            dayOfMonth: day,
+            date: date.toLocaleDateString(),
+            year: date.getFullYear(),
+            month: date.toLocaleDateString('en-US', { month: 'long' }),
+        });
+    }
+        return dates;
+};
+
 const RightPage = () => {
+    const [dates, setDates] = React.useState([])
+    const swiperRef = React.useRef(null);
+    const swiperRef2 = React.useRef(null);
+
+    const [months, setMonths] = React.useState([]);
+
+    React.useEffect(() => {
+        const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+        ];
+
+        setMonths(monthNames);
+    }, []);
+
+    function goToPrevious() {
+        swiperRef.current.swiper.slidePrev()
+    }
+
+    function goToNext() {
+        swiperRef.current.swiper.slideNext()
+    }
+
+    function goToPrevious2() {
+        swiperRef2.current.swiper.slidePrev()
+    }
+
+    function goToNext2() {
+        swiperRef2.current.swiper.slideNext()
+    }
+
+    const [activeCal, setActiveCal] = React.useState(0)
+
+    React.useEffect(() => {
+        const now = new Date()
+        const year = now.getFullYear()
+        const month = now.getMonth()
+
+        setDates(getDatesForMonth(year, month))
+    }, []);
+    console.log(dates)
+
     return (
         <div className='h-[100vh] flex flex-col gap-2'>
             <div className='flex justify-between items-center gap-2 mb-1'>
@@ -30,7 +90,7 @@ const RightPage = () => {
                 <Button startContent={<FaPlus />} className='bg-purple text-white'>Create a Request</Button>
             </div>
             <div>
-                <Card>
+                <Card className="w-full max-w-sm mx-auto">
                     <CardHeader className='flex justify-between items-center'>
                         <div className="flex justify-center items-center gap-2">
                             <FaCalendarCheck />
@@ -44,54 +104,66 @@ const RightPage = () => {
                     </CardHeader>
                     <CardBody>
                         <div className='mb-1 flex flex-col gap-3'>
-                            <div className='flex justify-between items-center bg-gray5 rounded-full'>
+                            <div className='flex justify-between items-center bg-gray5 rounded-full w-full'>
                                 <Button
                                     className='bg-white border-1 border-gray2 rounded-full'
                                     isIconOnly
                                     size="sm"
+                                    onClick={goToPrevious2}
                                     startContent={<MdOutlineKeyboardArrowLeft className='text-2xl' />}
                                 ></Button>
-                                <p className='font-semibold text-sm'>Aug, 2023</p>
+                                <Swiper
+                                    ref={swiperRef2}
+                                    slidesPerView={1}
+                                    spaceBetween={10}
+                                    loop={true}
+                                    navigation={true}
+                                    modules={[Pagination, Navigation]}
+                                >
+                                    {months.map((item, idx) => (
+                                        <SwiperSlide className='flex w-full' key={idx }>
+                                            <p className='w-full font-semibold text-sm flex items-center justify-center'>{item}, <span className='ml-1'>2023</span></p>
+                                        </SwiperSlide>
+                                    ))}
+                                </Swiper>
                                 <Button
                                     className='bg-white border-1 border-gray2 rounded-full'
                                     isIconOnly
                                     size="sm"
+                                    onClick={goToNext2}
                                     startContent={<MdKeyboardArrowRight className='text-2xl' />}
                                 ></Button>
                             </div>
-                            <div className='flex justify-between items-center'>
+                            <div className='flex justify-center items-center'>
                                 <Button
                                     className='bg-white border-1 border-gray2 rounded-full'
                                     isIconOnly
                                     size="sm"
+                                    onClick={goToPrevious}
                                     startContent={<MdOutlineKeyboardArrowLeft className='text-2xl' />}
                                 ></Button>
-                                <div className='flex justify-between items-center gap-6'>
-                                    <div className='flex justify-center items-center font-bold flex-col'>
-                                        <p className="font-bold text-gray2 text-xs">Fri</p>
-                                        <span>31</span>
-                                    </div>
-                                    <div className='flex justify-center items-center font-bold flex-col'>
-                                        <p className="font-bold text-gray2 text-xs">Sat</p>
-                                        <span>01</span>
-                                    </div>
-                                    <div className='flex justify-center items-center font-bold flex-col bg-purple text-white px-2 py-1 rounded-lg'>
-                                        <p className="font-bold text-white text-xs">Sun</p>
-                                        <span>02</span>
-                                    </div>
-                                    <div className='flex justify-center items-center font-bold flex-col'>
-                                        <p className="font-bold text-gray2 text-xs">Mon</p>
-                                        <span>03</span>
-                                    </div>
-                                    <div className='flex justify-center items-center font-bold flex-col'>
-                                        <p className="font-bold text-gray2 text-xs">Tue</p>
-                                        <span>04</span>
-                                    </div>
-                                </div>
+                                <Swiper
+                                    ref={swiperRef}
+                                    slidesPerView={5}
+                                    spaceBetween={10}
+                                    loop={true}
+                                    navigation={true}
+                                    modules={[Pagination, Navigation]}
+                                    className='w-full flex justify-between items-center px-2'
+                                >
+                                    {dates.map((date, idx) => (
+                                        <SwiperSlide className={`flex justify-center items-center font-bold flex-col cursor-pointer transition-all duration-300 ease-in-out ${activeCal === idx ? 'bg-purple text-white rounded-lg' : 'text-gray2'}`} onClick={() => setActiveCal(idx)} key={idx}
+                                        >
+                                            <p className={`font-bold text-xs ${activeCal === idx ? 'text-white' : 'text-gray2'}`}>{date.dayOfWeek}</p>
+                                            <span className={`text-lg ${activeCal === idx ? 'text-white' : 'text-black'}`}>{date.dayOfMonth}</span>
+                                        </SwiperSlide>
+                                    ))}
+                                </Swiper>
                                 <Button
                                     className='bg-white border-1 border-gray2 rounded-full'
                                     isIconOnly
                                     size="sm"
+                                    onClick={goToNext}
                                     startContent={<MdKeyboardArrowRight className='text-2xl' />}
                                 ></Button>
                             </div>
@@ -111,7 +183,7 @@ const RightPage = () => {
                                 <Tab
                                     title={
                                         <div className='flex justify-center items-center gap-2'>
-                                            <TiMessages />
+                                            <TiMessages/>
                                             <span>Meetings</span>
                                         </div>
                                     }
